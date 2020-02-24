36aeabd1a187d20b4195c9972feed149a6a80022
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-02-24 15:07:13 -0600

    Added prop check for products

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ProductConsumer } from '../context';
+import PropTypes from 'prop-types';
 
 export default class Product extends Component {
   render() {
@@ -49,6 +50,18 @@ export default class Product extends Component {
     );
   }
 }
+
+//using this as a way to check that the data is of correct type
+//Remember to check console for errors
+Product.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    img: PropTypes.string,
+    title: PropTypes.string,
+    price: PropTypes.number,
+    inCart: PropTypes.bool
+  }).isRequired
+};
 
 const ProductWrapper = styled.div`
   .card {
