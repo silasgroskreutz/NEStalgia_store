94ee7808def6ec6b75a9d20c8e68a6b422e7e9de
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-02-27 13:53:07 -0600

    CartItem and CartList components completed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import Title from '../Title';
 import CartColumns from './CartColumns.js';
 import EmptyCart from './EmptyCart';
 import { ProductConsumer } from '../../context';
+import CartList from './CartList';
 
 export default class Cart extends Component {
   render() {
@@ -16,6 +17,7 @@ export default class Cart extends Component {
                 <React.fragment>
                   <Title name='your' title='cart' />
                   <CartColumns />
+                  <CartList value={value} />
                 </React.fragment>
               );
             } else {
