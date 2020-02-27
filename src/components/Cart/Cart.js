156e93570e002ede5f773a04e17b5391c384aaf9
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-02-27 13:01:35 -0600

    Cart columns and Empty Cart components created

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import Title from '../Title';
+import CartColumns from './CartColumns.js';
+import EmptyCart from './EmptyCart';
+import { ProductConsumer } from '../../context';
 
 export default class Cart extends Component {
   render() {
     return (
       <section>
-        <Title name='your' title='cart' />
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.fragment>
+                  <Title name='your' title='cart' />
+                  <CartColumns />
+                </React.fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
       </section>
     );
   }
