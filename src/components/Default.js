112eed0de9fb36b3e66c63bdc00f0e85092994e8
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-02-28 13:57:54 -0600

    Added error image to default page

--- src/components/Default.js
+++ src/components/Default.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import img from '../I_am_Error.png';
 
 export default function Default(props) {
   return (
     <div className='container'>
       <div className='row'>
         <div className='col-10 mx-auto text-center text-title text-uppercase pt-5'>
+          <img src={img} className='img-fluid' alt='error' />
           <h1 className='display-3'>404</h1>
           <h1>error</h1>
           <h2>page not found</h2>
           <h3>
-            the requested URL{' '}
+            the requested URL
             <span className='text-danger'>"{props.location.pathname}"</span> was
             not found
           </h3>
